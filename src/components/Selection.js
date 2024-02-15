3629e32ecdaf1c4b8ea61c45644321dea6493fd4
Vidya <vidyasba@gmail.com>
2024-02-15 12:12:53 +0530

    done

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,15 @@
+import { useState } from "react"
 
+const Selection=({applyColor})=>{
+    const {bgColor,setBgColor} =useState("");
+
+    const applyColor =(background) =>{
+        setBgColor(background);
+    }
+    const changeColor =() =>{
+        applyColor(applyBgColor);
+    }
+return <div style={{width:200,height:300,border:"1px solid gray"}} onClick={changeColor}>
+<p>Selection</p>
+</div>
+}
